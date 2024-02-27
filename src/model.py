b21b5dd0280d4d3ffb4f01b433491c74222f214c
sharonncheng <scsharon@stanford.edu>
2024-02-27 14:20:23 -0800

    added GTSRB and some folders

---
+++ src/model.py
@@ -0,0 +1,19 @@
+import tensorflow as tf
+from tensorflow.keras import layers, models
+
+def create_cnn_model(input_shape, num_classes):
+    model = models.Sequential([
+        layers.Conv2D(32, (3, 3), activation='relu', input_shape=input_shape),
+        layers.MaxPooling2D((2, 2)),
+        layers.Conv2D(64, (3, 3), activation='relu'),
+        layers.MaxPooling2D((2, 2)),
+        layers.Conv2D(64, (3, 3), activation='relu'),
+        layers.Flatten(),
+        layers.Dense(64, activation='relu'),
+        layers.Dense(num_classes, activation='softmax')
+    ])
+    
+    model.compile(optimizer='adam',
+                  loss='sparse_categorical_crossentropy',
+                  metrics=['accuracy'])
+    return model
